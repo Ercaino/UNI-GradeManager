77b433d71cb0f4c1ada77cb39dc1c20163722a99
Xitonight <xitonight@gmail.com>
2025-05-28 20:10:40 +0200

    feat(colors): add color palette example file

---
+++ src/colors_example.cpp
@@ -0,0 +1,8 @@
+const int screenWidth = 1600;
+const int screenHeight = 900;
+const int fontSize = 20;
+
+const unsigned int backgroundColor = 0x1e1e2eff;
+const unsigned int textColor = 0xcdd6f4ff;
+const unsigned int buttonBackgroundColor = 0x181825ff;
+const unsigned int buttonBorderColor = 0xeba0acff;
